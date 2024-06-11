c78aa329c098d7b9dfb75ba73c46762c6a152570
Supawit <supawit.ma@gmail.com>
2024-06-11 13:54:09 +0700

    Update server.js

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use("/api/v1/rooms", rooms)
 app.use("/api/v1/cash-back", cashBack)
 
 // setting up port ------------------------------------------------------------------------
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5000
 const server = app.listen(
   PORT,
   console.log(
