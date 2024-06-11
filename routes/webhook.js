57541fcbf955e61daf6a7dcd8341c471a1ef5cf7
Supawit <supawit.ma@gmail.com>
2024-06-11 15:16:38 +0700

    change many thing

--- routes/webhook.js
+++ routes/webhook.js
@@ -10,36 +10,7 @@ getWebhook = (req, res) => {
 
 postWebhook = (req, res) => {
   const mess = Message.create({ message: req.body.events[0].message.text })
-
-  let reply_token = req.body.events[0].replyToken
-  let headers = {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${process.env.LINE_CHANNEL_ACCESS_TOKEN}`,
-  }
-  let body = JSON.stringify({
-    replyToken: reply_token,
-    messages: [
-      {
-        type: "text",
-        text: "Hello",
-      },
-      {
-        type: "text",
-        text: "How are you?",
-      },
-    ],
-  })
-  request.post(
-    {
-      url: "https://api.line.me/v2/bot/message/reply",
-      headers: headers,
-      body: body,
-    },
-    (err, res, body) => {
-      console.log("status = " + res.statusCode)
-    }
-  )
-  res.sendStatus(200)
+  res.sendStatus(200).json({ success: true, data: mess })
 }
 
 router.route("/").get(getWebhook).post(postWebhook)
