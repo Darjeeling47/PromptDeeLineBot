3dd9785f1bb74d3a66be6f1205b67f84d948e274
Supawit <supawit.ma@gmail.com>
2024-06-11 15:04:34 +0700

    production

--- routes/webhook.js
+++ routes/webhook.js
@@ -6,34 +6,34 @@ getWebhook = (req, res) => {
 }
 
 postWebhook = (req, res) => {
-  // let reply_token = req.body.events[0].replyToken
-  // let headers = {
-  //   "Content-Type": "application/json",
-  //   Authorization: `Bearer ${process.env.LINE_CHANNEL_ACCESS_TOKEN}`,
-  // }
-  // let body = JSON.stringify({
-  //   replyToken: reply_token,
-  //   messages: [
-  //     {
-  //       type: "text",
-  //       text: "Hello",
-  //     },
-  //     {
-  //       type: "text",
-  //       text: "How are you?",
-  //     },
-  //   ],
-  // })
-  // request.post(
-  //   {
-  //     url: "https://api.line.me/v2/bot/message/reply",
-  //     headers: headers,
-  //     body: body,
-  //   },
-  //   (err, res, body) => {
-  //     console.log("status = " + res.statusCode)
-  //   }
-  // )
+  let reply_token = req.body.events[0].replyToken
+  let headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${process.env.LINE_CHANNEL_ACCESS_TOKEN}`,
+  }
+  let body = JSON.stringify({
+    replyToken: reply_token,
+    messages: [
+      {
+        type: "text",
+        text: "Hello",
+      },
+      {
+        type: "text",
+        text: "How are you?",
+      },
+    ],
+  })
+  request.post(
+    {
+      url: "https://api.line.me/v2/bot/message/reply",
+      headers: headers,
+      body: body,
+    },
+    (err, res, body) => {
+      console.log("status = " + res.statusCode)
+    }
+  )
   res.sendStatus(200)
 }
 
