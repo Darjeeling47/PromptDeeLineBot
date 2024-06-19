a5bd704eaa7eb122e802d2d3d42110800bc37336
Supawit <supawit.ma@gmail.com>
2024-06-19 14:36:03 +0700

    deploy

--- utils/dateFormatter.js
+++ utils/dateFormatter.js
@@ -1,11 +1,7 @@
 const dateFormatter = (date) => {
   const newDate = new Date(date)
 
-  const result = newDate.toLocaleDateString("th-TH", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  })
+  const result = newDate.toLocaleDateString("th-TH")
 
   return result
 }
